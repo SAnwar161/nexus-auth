b0ccd038aeb99b19cad987ae9a9ba7c66cfcdb4e
SAnwar161 <sadat161@gmail.com>
2025-09-20 21:41:51 +0500

    🔨 Force GitHub sync override

--- src/index.js
+++ src/index.js
@@ -202,3 +202,4 @@ async function fetchHandler(request, env, ctx) {
 
 export default { fetch: fetchHandler };ECHO is on.
 // ?? GitHub sync trigger - 21:40 PKT 
+// ?? GitHub sync override - 21:45 PKT 
